931e42bc1b6ea17195421586ce3ab07606749347
alfinsuryaS <benjaminstwo@gmail.com>
2019-12-22 10:05:48 +0800

    Update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,72 @@
 #![allow(unused_variables)]
+#![allow(unused_imports)]
 
-use git2::{Oid, Repository};
+use git2::{Oid, Repository, ObjectType, Commit, BranchType};
 
 // -> Open & Select Your Repository
-pub fn open_repo() -> Repository {
+fn open_repo() -> Repository {
     let path = std::env::args().nth(1).unwrap_or(".".to_string());
     Repository::open(path.as_str()).expect("Couldn't open repo")
 }
 
 // -> Initial Head & Target
-pub fn initial_head() -> Oid {
+fn initial_head() -> Oid {
     let repo = open_repo();
     let initial_head = repo.head().unwrap();
     initial_head.target().unwrap()
 }
 
-// -> View all fullcommit log/detail
+// -> Convert from String to str
+fn convert_to_str(target: Option<&str>) -> &str {
+    target.unwrap_or("add")
+}
+
+// -> Commit Logging
 pub fn get_fullcommit_log() {
     let repo = open_repo();
     let commit = repo.find_commit(initial_head()).unwrap();
     
     let timestamp = commit.time().seconds();
     let tm = time::at(time::Timespec::new(timestamp, 0));
+    let time = tm.rfc822();
 
-    let commit_message = commit.message();
-    let commit_message_result = commit_message.as_deref().unwrap_or("Default String");
-    
+    let commit_message = convert_to_str(commit.message());
+    let commit_message_bytes = commit.message_bytes();
+    let commit_raw_header = convert_to_str(commit.raw_header());
+    let commit_summary = commit.summary();
+
     let commit_id = commit.id();
 
     let commit_author = commit.author();
-    let commit_author_name = commit_author.name();
-    let commit_author_email = commit_author.email();
-    let commit_committer_name = commit.committer();
+    let commit_author_name = convert_to_str(commit_author.name());
+    let commit_author_email = convert_to_str(commit_author.email());
+
+    let commiter = commit.committer();
+    let commiter_name = convert_to_str(commiter.name());
+    let commiter_email = convert_to_str(commiter.email());
 
 
     println!(
-        "Commit Message Log: \n {:#?}",
-        commit_message_result 
+        "\n Last Commit:\n Id -> {} \n Message -> {} Committer Name -> {} \n Committer Email -> {} \n Time -> {} \n Raw -> {:#?} \n",
+        commit_id, 
+        commit_message, 
+        commiter_name,
+        commiter_email,
+        time,
+        commit_raw_header,
     )
+}
+
+
+// -> Repo Logging
+pub fn get_fullrepo_log() {
+    let repo = open_repo();
+    let commit = repo.find_commit(initial_head()).unwrap();
+    let mut branch = repo.branch("test_branch", &commit, true).unwrap();
+    // let mut find = repo.find_branch("test_branch",BranchType::Local).unwrap();
+    let mut b1 = branch.rename("test_branch_1", true).unwrap();
+    b1.set_upstream(Some("master")).unwrap();
+    b1.upstream().unwrap();
+
+    assert_eq!(b1.name().unwrap(), Some("test_branch_1"))
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ extern crate git2;
 mod lib;
 
 fn main() {
-    lib::get_fullcommit_log()
+    lib::get_fullcommit_log();
+    lib::get_fullrepo_log();
 }
